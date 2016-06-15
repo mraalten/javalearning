4a8de1b02d82dad7ad09438b788808dc9d6a1aaf
edwincox <edje_coxje@hotmail.com>
2016-06-15 16:13:15 +0200

    verwijderd person.java nieuw geboortedatum.java
    Wanneer je het programma start heb ik getter  + constructor gemaakt van de class  geboortedatum. deze geeft dan weer de datum terug.

--- src/main/java/nl/aalten/javalearning/Main.java
+++ src/main/java/nl/aalten/javalearning/Main.java
@@ -1,20 +1,23 @@
 package nl.aalten.javalearning;
 
-import java.util.Date;
-
 public class Main {
 
     public static void main(String[] args) {
 
         Person edwin = new Person("Edwin", "Goudsmidstraat");
-        Leeftijd test = new Leeftijd("06/06/1984");
-        Date geboorteDatum = new Date();
+
+        //Date geboortejaarmaanddag = new Date("6/6/1984");
+        geboortedatum geboortejaarmaanddag = new geboortedatum(6 ,6 ,1984);
+
+        //SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        //System.out.println("Yes: " + sdf.format(poep));
+
+        //geboortedatum test = new geboortedatum("06/06/1984");
+        //Date geboorteDatum = new Date();
+
         System.out.println("Mijn naam is " + edwin.getNaam());
         System.out.println("Mijn adres is " + edwin.getAdres());
-
-        System.out.println("Mijn adres is " + test.getOud());
-
-        //
+        System.out.println("Mijn geboortedatum is " + geboortejaarmaanddag.getDag() + "/" + geboortejaarmaanddag.getMaand() + "/" + geboortejaarmaanddag.getJaar());
 
     }
 
