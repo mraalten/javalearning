797dbb446781c390fd471e464f3f652a87307d89
edwincox <edje_coxje@hotmail.com>
2016-06-22 15:19:36 +0200

    opdracht 1 gemaakt.

--- src/main/java/nl/aalten/javalearning/Main.java
+++ src/main/java/nl/aalten/javalearning/Main.java
@@ -4,21 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Person edwin = new Person("Edwin", "Goudsmidstraat");
+        Person edwin = new Person();
 
-        //Date geboortejaarmaanddag = new Date("6/6/1984");
-        geboortedatum geboortejaarmaanddag = new geboortedatum(6 ,6 ,1984);
+        edwin.setNaam("Edwin");
+        edwin.setStraatnaam("Goudsmidstraat");
+        edwin.setHuisnummer("12");
+        edwin.setHuisnummerToevoeging("AB");
+        edwin.setPostcode("5811 PP");
+        edwin.setWoonplaats("Venray");
 
-        //SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        //System.out.println("Yes: " + sdf.format(poep));
 
-        //geboortedatum test = new geboortedatum("06/06/1984");
-        //Date geboorteDatum = new Date();
-
-        System.out.println("Mijn naam is " + edwin.getNaam());
-        System.out.println("Mijn adres is " + edwin.getAdres());
-        System.out.println("Mijn geboortedatum is " + geboortejaarmaanddag.getDag() + "/" + geboortejaarmaanddag.getMaand() + "/" + geboortejaarmaanddag.getJaar());
-
+        System.out.println("Naam: " + edwin.getNaam());
+        System.out.println("Straatnaam " + edwin.getStraatnaam());
+        System.out.println("Huisnummer " + edwin.getHuisnummer());
+        System.out.println("HuisnummerToevoeging " + edwin.getHuisnummerToevoeging());
+        System.out.println("Postcode " + edwin.getPostcode());
+        System.out.println("Woonplaats: " + edwin.getWoonplaats());
     }
 
 }
--- src/main/java/nl/aalten/javalearning/Person.java
+++ src/main/java/nl/aalten/javalearning/Person.java
@@ -1,20 +1,65 @@
 package nl.aalten.javalearning;
 
 public class Person {
+
     private String naam;
-    private String adres;
+    private String Straatnaam;
+    private String Huisnummer;
+    private String HuisnummerToevoeging;
+    private String Postcode;
+    private String Woonplaats;
 
 
-    public Person(String naam, String adres) {
+    public void setNaam(String naam)
+    {
         this.naam = naam;
-        this.adres = adres;
     }
 
     public String getNaam() {
-        return naam;
+        return this.naam;
     }
 
-    public String getAdres() {
-        return adres;
+    public void setStraatnaam(String Straatnaam){
+        this.Straatnaam = Straatnaam;
+    }
+
+    public String getStraatnaam() {
+        return this.Straatnaam;
+    }
+
+    public void setHuisnummer(String Huisnummer)
+    {
+        this.Huisnummer = Huisnummer;
+    }
+
+    public String getHuisnummer() {
+        return this.Huisnummer;
+    }
+
+    public void setHuisnummerToevoeging(String HuisnummerToevoeging)
+    {
+        this.HuisnummerToevoeging = HuisnummerToevoeging;
+    }
+
+    public String getHuisnummerToevoeging() {
+        return this.HuisnummerToevoeging;
+    }
+
+    public void setPostcode(String Postcode)
+    {
+        this.Postcode = Postcode;
+    }
+
+    public String getPostcode() {
+        return this.Postcode;
+    }
+
+    public void setWoonplaats(String Woonplaats)
+    {
+        this.Woonplaats = Woonplaats;
+    }
+
+    public String getWoonplaats() {
+        return this.Woonplaats;
     }
 }
