3c86f62665b8426831d0d586beddecb6c8d71404
edwincox <edje_coxje@hotmail.com>
2016-07-31 17:02:09 +0200

    opdracht 1 gemaakt. verbetering.

--- src/main/java/nl/aalten/javalearning/Main.java
+++ src/main/java/nl/aalten/javalearning/Main.java
@@ -63,8 +63,4 @@ public class Main {
         return correspondentieAdres;
 
     }
-
-
-
-
 }
--- src/main/java/nl/aalten/javalearning/Person.java
+++ src/main/java/nl/aalten/javalearning/Person.java
@@ -40,6 +40,4 @@ public class Person {
         return this.correspondentieAdres = correspondentieAdres;
     }
 
-
-
 }
