4ecd9f7bb468b2a2cbc4abfc06919eb8f5d4a57c
CoxEGTPH <Edwin.Cox@rabobank.nl>
2016-08-08 20:38:04 +0200

    Samen met Richard

--- src/test/java/nl/aalten/javalearning/PersonTest.java
+++ src/test/java/nl/aalten/javalearning/PersonTest.java
@@ -1,5 +1,7 @@
 package nl.aalten.javalearning;
 
+import org.junit.Test;
+
 import java.time.LocalDate;
 
 import static org.hamcrest.core.Is.is;
@@ -7,8 +9,33 @@ import static org.junit.Assert.assertThat;
 
 public class PersonTest {
 
+    @Test
+    (expected = IllegalStateException.class)
+    public void it_should_do_nothing_when_it_is_0(){
+        Person person = new Person("Fred", "4545", 0);
+    }
 
-//    @Test
+    @Test
+    (expected = IllegalStateException.class)
+    public void it_should_do_nothing_when_it_is_negative_number(){
+        Person person = new Person("Fred", "4545", -9);
+    }
+
+    @Test
+    (expected = IllegalStateException.class)
+    public void it_should_do_nothing_when_there_is_not_a_accountnumber(){
+        Person person = new Person("Fred");
+    }
+
+    @Test
+
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    //    @Test
     public void it_should_return_the_correct_age_because_no_birthday_yet() {
         Person person = getValidPersonObject();
         person.setGeboorteDatum(LocalDate.of(1970, 12, 31));
