f72cb1b35bb88cc3df5fb02dcc2599b4b9773e32
edwincox <info@edwincox.nl>
2016-11-18 10:48:26 +0100

    Nieuwe functionaliteit gebouw, je mag rood staan.

--- src/main/java/nl/oefen/cox/HypotheekDossier.java
+++ src/main/java/nl/oefen/cox/HypotheekDossier.java
@@ -9,11 +9,37 @@ public class HypotheekDossier {
 
     private int id;
     private Persoon persoon;
+    private Annuiteit annuiteit;
+    private Linear linear;
 
-    public void addLening(String naam, LocalDate geboortedatum, String adresgegevens){
-        persoon = new Persoon(naam,  geboortedatum, adresgegevens);
+    public int addPersoon(String naam, LocalDate geboorteDatum, String straatNaam){
+      persoon = new Persoon(naam, geboorteDatum, straatNaam);
+
+        return id;
+    }
+
+    public void addLening(int bedrag, int termijnenAnnuiteit){
+        annuiteit = new Annuiteit(bedrag, termijnenAnnuiteit);
+    }
+
+
+    public void addLineaire(int bedrag, int looptijd){
+        linear = new Linear(bedrag, looptijd);
 
     }
+
+//    public void addPersoon(String naam, LocalDate geboorteDatum, String straatNaam){
+//        persoon = new Persoon(naam, geboorteDatum, straatNaam);
+//    }
+
+
+//    public int addLening(int bedrag, int termijnenAnnuiteit){
+//        annuiteit = new Annuiteit(bedrag, termijnenAnnuiteit);
+//
+//
+//        return leningDeel;
+//    }
+
 
     public void addWoning(String naam, LocalDate geboortedatum, String adresgegevens){
        persoon = new Persoon(naam,  geboortedatum,  adresgegevens);
