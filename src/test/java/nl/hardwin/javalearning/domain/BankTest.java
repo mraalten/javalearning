cb2adad6749e7e9cefbebe60f3e14e2db6a5a2d6
edwincox <info@edwincox.nl>
2017-01-18 20:45:20 +0100

    bank

--- src/test/java/nl/hardwin/javalearning/domain/BankTest.java
+++ src/test/java/nl/hardwin/javalearning/domain/BankTest.java
@@ -14,49 +14,82 @@ public class BankTest {
     @Test
     public void it_should_open_a_betaalrekening() {
         Bank testbank = new Bank();
-        String var = testbank.openBetaalrekening("BSN112233", "Edwin Betaalrekening", LocalDate.of(1988, 6, 3), 2000, 500);
-        System.out.println(var);
-        Assert.assertNotNull(var, "This object should not be null");
+        Betaalrekening betaalrekeningEdwin = testbank.openBetaalrekening("11111111", "Edwin Test", LocalDate.of(1984, 6, 6),2000, 400);
+        Assert.assertNotNull(String.valueOf(betaalrekeningEdwin), "This object should not be null");
     }
 
     @Test
     public void it_should_open_a_spaarrekening() {
         Bank testbank = new Bank();
-        String var = testbank.openSpaarrekening("BSN112233", "Edwin Betaalrekening", LocalDate.of(1988, 6, 3), 2000, 500);
-        System.out.println(var);
-        Assert.assertNotNull(var, "This object should not be null");
+        Spaarrekening spaarrekekingTest = testbank.openSpaarrekening("BSN112233", "Test Spaarrekening", LocalDate.of(1988, 6, 3), 2000, 500);
+
+        Assert.assertNotNull(String.valueOf(spaarrekekingTest), "This object should not be null");
     }
 
     @Test
-    public void it_should_depoit_money_to_betaalrekening(){
+        public void it_should_depoit_money_to_betaalrekening(){
         Bank testbank = new Bank();
-        String TestRekening = testbank.openBetaalrekening("BSN112233", "Edwin Betaalrekening", LocalDate.of(1988, 6, 3), 2000, 500);
+        Betaalrekening betaalrekeningTest = testbank.openBetaalrekening("BSN112233", "Edwin Betaalrekening", LocalDate.of(1988, 6, 3), 2000, 500);
 
-        testbank.deposit(TestRekening, 500);
+        testbank.deposit(betaalrekeningTest.getRekeningNummer(), 500);
 
-        assertThat(testbank.getSaldo(TestRekening), is(2500));
+        assertThat(betaalrekeningTest.getSaldo(), is(2500));
     }
 
     @Test
     public void it_should_withdraw_money_from_betaalrekening_to_spaarrekening(){
         Bank testbank = new Bank();
-        String TestRekeningBetaalrekening = testbank.openBetaalrekening("BSN112233", "Edwin Betaalrekening", LocalDate.of(1988, 6, 3), 2000, 500);
-        String TestRekeningSpaarrekening = testbank.openSpaarrekening("BSN112233", "Edwin Spaarrekening", LocalDate.of(1988, 6, 3), 500, 0);
+        Betaalrekening betaalrekeningTest = testbank.openBetaalrekening("BSN112233", "Edwin Betaalrekening", LocalDate.of(1988, 6, 3), 2000, 500);
 
-        testbank.transferMoney(TestRekeningBetaalrekening, TestRekeningSpaarrekening, 200);
+        testbank.withdraw(betaalrekeningTest.getRekeningNummer(), 800);
 
-        assertThat(testbank.getSaldo(TestRekeningBetaalrekening), is(1800));
-        assertThat(testbank.getSaldo(TestRekeningSpaarrekening), is(500));
+
+        assertThat(betaalrekeningTest.getSaldo(), is(1200));
     }
 
     @Test
     public void it_should_print_transacties_from_betaalrekening() {
         Bank testbank = new Bank();
-        String TestRekeningBetaalrekening = testbank.openBetaalrekening("BSN112233", "Edwin Betaalrekening", LocalDate.of(1988, 6, 3), 2000, 500);
+        Betaalrekening betaalrekeningTest = testbank.openBetaalrekening("BSN112233", "Edwin Betaalrekening", LocalDate.of(1988, 6, 3), 2000, 500);
 
-        testbank.printOverzichtTransacties(TestRekeningBetaalrekening);
+        betaalrekeningTest.printTransactie();
+    }
+
+    @Test
+    public void it_should_give_the_saldo_of_account_back(){
+        Bank testbank = new Bank();
+        Betaalrekening betaalrekeningTest = testbank.openBetaalrekening("BSN112233", "Edwin Betaalrekening", LocalDate.of(1988, 6, 3), 2000, 500);
+
+        assertThat(betaalrekeningTest.getSaldo(), is(2000));
 
     }
 
+    @Test
+    public void it_should_print_the_transacties(){
+        Bank testbank = new Bank();
+        Betaalrekening betaalrekeningTest = testbank.openBetaalrekening("BSN112233", "Edwin Betaalrekening", LocalDate.of(1988, 6, 3), 2000, 500);
+
+
+        testbank.printOverzichtTransacties(betaalrekeningTest.getRekeningNummer());
+    }
+
+    @Test
+    public void it_should_transer_money_to_a_other_account(){
+        Bank testbank = new Bank();
+        Betaalrekening betaalrekeningTest1 = testbank.openBetaalrekening("BSN112233", "Edwin Betaalrekening", LocalDate.of(1988, 6, 3), 2000, 500);
+        Betaalrekening betaalrekeningTest2 = testbank.openBetaalrekening("BSN787889", "test Test", LocalDate.of(1988, 6, 3), 2000, 500);
+
+        testbank.transferMoney(betaalrekeningTest1.getRekeningNummer(), betaalrekeningTest2.getRekeningNummer(), 400);
+
+        assertThat(betaalrekeningTest1.getSaldo(), is(1600));
+        assertThat(betaalrekeningTest2.getSaldo(), is(2400));
+    }
+
+    @Test
+    public void it_should_(){
+        Bank testbank = new Bank();
+
+        testbank.getAllRekeningen();
+    }
 
     }
